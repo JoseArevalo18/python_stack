b83b55d5d5a35f894e355c783bbf0f3b2a4f9d22
JoseArevalo18 <josealfredoee@hotmail.es>
2021-05-31 22:05:15 -0500

    update

---
+++ my_enviroments/TRABAJOS/hello_word.py
@@ -0,0 +1,16 @@
+saludo= "Hola mundo"
+# # # 1. TAREA: imprimir "Hola mundo"
+print(saludo)
+# # # 2. imprimir "Hola Noelle!" con el nombre en una variable
+name = "Noelle!"
+print( "Hola", name)	# con una coma
+print( "Hola" + name)	# con un +
+# # # # 3. imprimir "Hola 42!" con un numero en una variable
+name1 = 42
+print(f"Hola,{name1}!")	# con una coma
+# print("Hola"+name1)	# con un + - !Este debería darnos un error!
+# # 4. imprimir "Me encanta comer sushi and pizza." con los alimentos en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("Me encanta comer",fave_food1,"and",fave_food2) # con .format()
+print(f"Me encanta comer {fave_food1} and {fave_food2}") # con una cadena f
