16b4ddfdd1640042ef45dafd5e3c021ac805cfde
JoseArevalo18 <josealfredoee@hotmail.es>
2021-06-03 22:17:01 -0500

    update

---
+++ Python/fundamentals/Operador ternario.py
@@ -0,0 +1,8 @@
+stacks=5
+                        # tradicional
+if stacks >= 3:
+    print('Coding Dojo')
+else:
+    print('You are not Coding Dojo!')
+                        # ternario
+print('Coding Dojo' if stacks >= 3 else 'You are not Coding Dojo!')
