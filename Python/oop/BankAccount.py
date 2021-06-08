251653836215e97a9fde42e17537e8d10df3fcd5
JoseArevalo18 <josealfredoee@hotmail.es>
2021-06-07 21:29:30 -0500

    UPDATE

---
+++ Python/oop/BankAccount.py
@@ -0,0 +1,31 @@
+class bankAccount:
+	def __init__(self,int_rate,balance:0):
+		self.int_rate = int_rate
+		self.balance = balance
+
+	def deposito(self,amount):
+		self.balance += amount
+		return self
+
+	def retiro(self,amount):
+		if amount > self.balance:
+			print("Fondos insuficientes: cobrar una tarifa de 5$")
+			self.balance -= 5
+		else:
+			self.balance -= amount
+		return self
+
+	def display_account_info(self):
+		print("Saldo: ", self.balance)
+		return self
+
+	def yield_interest(self):
+		if self.balance > 0:
+			self.balance += self.balance * self.int_rate
+		return self
+#USUARIOS
+cuenta1 = bankAccount(0.08,200)
+cuenta2 = bankAccount(0.04,500)
+#DEPOSITIOS Y RETIROS
+cuenta1.deposito(100).deposito(100).deposito(200).retiro(500).yield_interest().display_account_info()
+cuenta2.deposito(200).deposito(150).retiro(900).retiro(100).retiro(50).retiro(50).yield_interest().display_account_info()
