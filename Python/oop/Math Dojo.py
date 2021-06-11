0af5affa5ec42a868aed9bed50cb7202fcca2222
JoseArevalo18 <josealfredoee@hotmail.es>
2021-06-11 11:28:03 -0500

    update

--- Python/oop/Math Dojo.py
+++ Python/oop/Math Dojo.py
@@ -13,9 +13,9 @@ class MathDojo:
             self.result -= nums[i]
         self.result -= num
         return self
-
+# crear una instruccion:
 md = MathDojo()
-
+# para probar:
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x) #debe imprimir 5
 # corre cada uno de los metodos algunos mas veces y valida el resultado!
