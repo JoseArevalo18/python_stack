3ddf3339607f1877408ead47f1f825f1fd213ceb
JoseArevalo18 <josealfredoee@hotmail.es>
2021-06-21 20:22:30 -0500

    update

--- Django/ejercicio en clase/DOJO_READS/main/models.py
+++ Django/ejercicio en clase/DOJO_READS/main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import re
+
+from django.db.models.base import Model
 # crea las variables que se van a utilizar en la aplicacion y las validaciones 
 # contienen los campos escenciales que se van a utilizar
 EMAIL_REGEX = re.compile(r'^[a-zA-z0-9.+_-]+@[a-zA-z0-9]+\.[a-zA-z]+$')
@@ -20,11 +22,34 @@ class UserManager(models.Manager):
             errors['pw_match'] = "Password does not match"
         return errors
 
+#MODELS CREATIONS
 class User(models.Model):
     name       = models.CharField(max_length=50)
     alias      = models.CharField(max_length=50)
     email      = models.CharField(max_length=50)
     password   = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at  = models.DateTimeField(auto_now=True)
+    objects    = UserManager()
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at  = models.DateTimeField(auto_now=True)
+    objects    = UserManager()
+
+class Author(models.Model):
+    name = models.CharField(max_length=75)
+    books = models.ManyToManyField(Book,related_name="authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at  = models.DateTimeField(auto_now=True)
+    objects    = UserManager()
+
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.IntegerField()
+    user_review = models.ForeignKey(User, related_name="user_review", on_delete=models.CASCADE)
+    book_reviewed = models.ForeignKey(Book, related_name="book_reviews", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at  = models.DateTimeField(auto_now=True)
     objects    = UserManager()
