3d0dcafb42e4a24e065d19cc905fae1012e488f4
JoseArevalo18 <josealfredoee@hotmail.es>
2021-06-23 20:25:05 -0500

    update

--- Django/ejercicio en clase/DOJO_READS/main/urls.py
+++ Django/ejercicio en clase/DOJO_READS/main/urls.py
@@ -5,7 +5,7 @@ from.import views
 urlpatterns=[
     path('',views.index),
     path('user/create_user',views.create_user),
-    # path('user/dashboard',views.dashboard),
+    path('user/dashboard',views.dashboard),
     path('user/login',views.login),
-    path('user/logout',views.logout),
+    path('user/logout',views.logout)
 ]
