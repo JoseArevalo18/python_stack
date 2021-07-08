cc6c9a4106756dc4773fa62f47bb17b3bbb1d8fa
JoseArevalo18 <josealfredoee@hotmail.es>
2021-07-08 15:51:57 -0500

    update

--- Django/django_orm/single_model_orm/users_app/models.py
+++ Django/django_orm/single_model_orm/users_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    
+    def __repr__(self):
+        return f"<Usuario object: {self.first_name} {self.last_name} {self.email_address} {self.age} ({self.id})"
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
