b5351eee3cd01683cd7b83f7dea44f49459e5eb7
JoseArevalo18 <josealfredoee@hotmail.es>
2021-07-12 21:22:09 -0500

    update

--- Python/fundamentals/Insertion Sort.py
+++ Python/fundamentals/Insertion Sort.py
@@ -1,13 +1,18 @@
 arr=[8,5,2,6,3,1,9,4,0,7]
 
-def bubblesort(arr):
-    count=0
-    for j in range (len(arr)-1):
-        for i in range (len(arr)-1-j):
+def bubbleinsort(arr):
+    for indice in range (1,len(arr)):
+        count=0
+        valor = arr[indice]
+        i = indice - 1
+        while i >=0:
             count+=1
-            if arr[i]>arr[i+1]:
-                arr[i], arr[i+1] =arr[i+1], arr[i]
-    print("# de interacciones:",count)
+            if valor < arr[i]:
+                arr[i+1]=arr[i]
+                arr[i]=valor
+                i=i-1
+            else:
+                break
+    print("#de interacciones",count)
     return arr
-
-print(bubblesort(arr))
+print(bubbleinsort(arr))
