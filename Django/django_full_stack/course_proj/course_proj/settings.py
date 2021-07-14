8f813bdefce378c6114a7b8b6be998130c49103c
JoseArevalo18 <josealfredoee@hotmail.es>
2021-07-14 16:46:45 -0500

    pdate

--- Django/django_full_stack/course_proj/course_proj/settings.py
+++ Django/django_full_stack/course_proj/course_proj/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses_app',
+    'course_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
