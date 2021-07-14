3e6fa1be6529b442c6f855a880d229ed61f511d6
JoseArevalo18 <josealfredoee@hotmail.es>
2021-07-14 16:40:47 -0500

    update

---
+++ Django/django_full_stack/course_proj/course_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('add/',views.addCourse),
+    path('courses/destroy/<int:number>',views.deleteCourse)
+    
+]
